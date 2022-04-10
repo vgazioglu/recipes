cdb03aafc090884147315e718c6b0026a61b366b
Volki Tolki <vgazioglu@gmail.com>
2022-04-10 15:48:01 +0300

    Detail page is now ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -65,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
               Navigator.push(
                 context, 
                 MaterialPageRoute(builder: (context) {
-                  return Text('Detail Page');
+                  return RecipeDetail(recipe: Recipe.samples[index]);
                 }
                 )
                 );
